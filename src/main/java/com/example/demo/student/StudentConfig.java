5d484e5cf81a593539aed83e06c6047d24c1e780
007Miles <milendrafernando@gmail.com>
2023-06-30 10:55:49 +0530

    Post data to database

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student miles = new Student(
+                    1L,
+                    "Miles",
+                    "miles@gmail.com",
+                    LocalDate.of(2000, Month.MAY, 31),
+                    23
+            );
+
+            Student axl = new Student(
+                    "Axl",
+                    "axl@gmail.com",
+                    LocalDate.of(2002, Month.FEBRUARY, 1),
+                    21
+            );
+
+            repository.saveAll(
+                    List.of(miles, axl)
+            );
+        };
+    }
+}
